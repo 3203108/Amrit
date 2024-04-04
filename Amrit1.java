e824f373796dd0a7ac63153b77797bb48a9de8c2
3203108 <166063780+3203108@users.noreply.github.com>
2024-04-04 15:43:09 -0400

    Add files via upload

---
+++ Amrit1.java
@@ -0,0 +1,26 @@
+package com.example.Amrit1;
+
+class student{
+	int age;
+	String name;
+	public void printAge() {
+	System.out.println("Age of student is" );
+	 	
+	}
+
+	
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
